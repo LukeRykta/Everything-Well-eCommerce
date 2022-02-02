75c717abeaec5c3eeef547c3759a4e921108b1e4
LukeRykta <nzp744@my.utsa.edu>
2022-02-02 16:55:07 -0600

    added .env with mLab connection definition updated api.js to include get, post, delete methods updated test.js to include mongoose db connection

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Todo = require('../models/todo');
 
 router.get('/todos', (req, res, next) => {
     // This will return all the data, exposing only the id and action field to the client
