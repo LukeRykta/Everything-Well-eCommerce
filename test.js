06b5dd00ab06692435994ca6707d830eff5b25ee
LukeRykta <nzp744@my.utsa.edu>
2022-02-02 15:06:38 -0600

    added https support

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const https = require('https');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = https.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
