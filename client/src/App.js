9c0c7a0040bd44d7dc079a33794650af24e04a57
LukeRykta <nzp744@my.utsa.edu>
2022-02-02 19:10:13 -0600

    updated website to form lists , add queries into database, and delete items

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './components/Todo';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Danny is a G.O.A.T.</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://pbs.twimg.com/media/CaeaiplUUAE0m0r.jpg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn about Extreme Programming
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Todo />
+      </div>
   );
-}
+};
 
 export default App;
