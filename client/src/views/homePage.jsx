4489b65de4ceb6e08758864574a91a577b54e3b3
LukeRykta <nzp744@my.utsa.edu>
2022-02-06 03:57:55 -0600

    added background.jpg

--- client/src/views/homePage.jsx
+++ client/src/views/homePage.jsx
@@ -3,20 +3,12 @@ import {Link} from "react-router-dom";
 
 const MainPage = () => {
     return(
-
-        <div className="App">
-            <h1>Welcome to the Home Page</h1>
-            <div className="App2">
-                <h2>Hope you enjoy your stay~</h2>
+        <div className="BackgroundImage">
+            <div className="Home">
+                <h1>Welcome to the Home Page</h1>
+                    <h2>Hope you enjoy your stay~</h2>
+                <Link to="/todoPage" className="link">Click here for TODO page</Link>
             </div>
-            <Link to="/todoPage">Click here for TODO page</Link>
-            <video loop autoPlay>
-                <source
-                    src='https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4'
-                    type='video/mp4'
-                />
-                Your browser does not support the video tag.
-            </video>
         </div>
 
     );
