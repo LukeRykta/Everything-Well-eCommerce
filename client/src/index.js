fb22c777082930c8c6cc1a876b4f4adb2f315c8b
LukeRykta <nzp744@my.utsa.edu>
2022-02-06 15:19:47 -0600

    created asset folder for background images

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ The function below is the root of our React front-end. the render function is constantly refreshing <App/>
+ which is linked to App.js . There we are routing our views and in our views we are updating our components.
+ This is the current hierarchy, but it may change depending on how we decide to move forward with page creation
+ and management. -Luke
+ */
 ReactDOM.render(
   <React.StrictMode>
     <App />
