1d2317fa7f3726a8edaa14b657ca0d5349d07f67
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-02-23 20:45:10 -0600

    login format update

--- client/src/views/pages/login.jsx
+++ client/src/views/pages/login.jsx
@@ -6,7 +6,6 @@ import hidePwdImg from '../assets/hide-password.png';
 import {Row} from "react-bootstrap";
 import "../css/Form.css"
 
-
 const LoginPage = () => {
     const [pwd, setPwd] = useState('');
     const [isRevealPwd, setIsRevealPwd] = useState(false);
@@ -46,53 +45,5 @@ const LoginPage = () => {
         </div>
     )
 }
+
 export default LoginPage;
-
-/*
-
-         <div className="container mt-5">
-            <form className="form">
-                <h2 className="text-center">Create New Item</h2>
-                <Row>
-                    <input
-                        name="title"
-                        onChange={handleChange}
-                        value={item.title}
-                        className="form-control"
-                        placeholder="title"
-                        type="text"
-                    />
-                    <input
-                        name="vendor"
-                        onChange={handleChange}
-                        value={item.vendor}
-                        className="form-control"
-                        placeholder="vendor"
-                        type="text"
-                    />
-                    <input
-                        name="image"
-                        onChange={handleChange}
-                        value={item.image}
-                        className="form-control"
-                        placeholder="image (url)"
-                        type="text"
-                    />
-                    <input
-                        name="price"
-                        onChange={handleChange}
-                        value={item.price}
-                        className="form-control"
-                        placeholder="price"
-                        type="text"
-                    />
-                    <button
-                        onClick={handleSubmit}
-                        className="btn btn-outline-success mt-3">
-                        Create
-                    </button>
-                </Row>
-            </form>
-        </div>
-
- */
