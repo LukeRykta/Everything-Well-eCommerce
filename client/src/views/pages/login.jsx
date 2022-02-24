1d9356a38d87191b2d4384b5f1405681361bf0c9
Danny <trungdupng5818@gmail.com>
2022-02-23 20:02:43 -0600

    adding show/hide password feature

--- client/src/views/pages/login.jsx
+++ client/src/views/pages/login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import showPwdImg from '../assets/show-password.png';
+import hidePwdImg from '../assets/hide-password.png';
+
 
 const LoginPage = () => {
+    const [pwd, setPwd] = useState('');
+    const [isRevealPwd, setIsRevealPwd] = useState(false);
+
     return(
         <div className="container col-11, mt-5 ">
             <h1>Please Log In</h1>
@@ -10,9 +17,18 @@ const LoginPage = () => {
                     <p>Username</p>
                     <input type="text"/>
                 </label>
-                <label className="col-11">
+                <label className="col-10">
                     <p className="mt-3">Password</p>
-                    <input type="text"/>
+                    <input
+                        type={isRevealPwd ? "text" : "password"}
+                        value={pwd}
+                        onChange={e => setPwd(e.target.value)}
+                    />
+                    <img
+                        title={isRevealPwd ? "Hide password" : "Show password"}
+                        src={isRevealPwd ? hidePwdImg : showPwdImg}
+                        onClick={() => setIsRevealPwd(prevState => !prevState)}
+                    />
                 </label>
                 <div className="col-9">
                     <button type="submit" className="btn btn-outline-secondary btn-outline-success mt-4">Submit</button>
