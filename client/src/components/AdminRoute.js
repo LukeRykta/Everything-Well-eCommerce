a1abe373f584b60fbbb4c7f11bc7cabae44ae523
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-04-06 22:16:27 -0500

    updated cache

--- client/src/components/AdminRoute.js
+++
@@ -1,8 +0,0 @@
-import { isAuthenticated } from "../services/authService";
-import { Outlet } from "react-router-dom";
-import LoginPage from "../views/pages/login";
-
-export default function AdminRoute() {
-    const user = isAuthenticated();
-    return user.role === 'ADMIN' ? <Outlet/> : <LoginPage/>;
-};
