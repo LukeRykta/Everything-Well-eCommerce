e488a63be482167a4ae29d07f4e996fdbf6da5da
Stephanie Younes <stephanie.younes@yahoo.com>
2022-04-17 19:12:59 -0500

    footer

--- client/src/views/pages/home.jsx
+++ client/src/views/pages/home.jsx
@@ -78,32 +78,124 @@ const MainPage = () => {
 
             </div>
         </div>
-        <div className="HomeFooter">
-        <div className="container-fluid">
-            <ul>
-            <div class="col-lg-4 mb-5 mb-lg-0">
-                        <p className="FooterHeaders">Around the Web</p>
-                       
-                        <FontAwesomeIcon icon={faFacebook} size="4x" className="icon" beat/>
-                        <FontAwesomeIcon icon={faTwitter} size="4x" className="icon" beat/>
-                        <FontAwesomeIcon icon={faInstagram} size="4x" className="icon" beat/>
 
-                        
 
-                        
-            </div>
             
             
             
 
 
-                    
+       
+  <div class="row">
+    	<div class="col-md-12">
+    	    <footer class="footer">				
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						About us
+					</div>	
+					<p class="white-text">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500 text of the printing.
+					</p>
+				</div>
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Latest themes
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Appointment
+						</a>
+						<a href="#">
+							Health center
+						</a>
+						<a href="#">
+							Quality
+						</a>
+						<a href="#">
+							Wallstreet
+						</a>
+					</div>
+				</div>
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Quick Links
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Blog
+						</a>
+						<a href="#">
+							FAQ
+						</a>
+						<a href="#">
+							Terms & conditions
+						</a>
+						<a href="#">
+							Privacy policy
+						</a>
+					</div>
+				</div>
+				<div class="col-md-3 m-b-30">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Support
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Affiliate
+						</a>
+						<a href="#">
+							Login
+						</a>
+						<a href="#">
+							All theme package
+						</a>
+						<a href="#">
+							Support forum
+						</a>
+					</div>
+
+					<div class="footer-social-links m-t-30">
+						<li>
+							<a href="#">
+								<i class="fa fa-facebook" aria-hidden="true"></i>
+							</a>
+							<a href="#">
+								<i class="fa fa-twitter" aria-hidden="true"></i>
+							</a>
+							<a href="#">
+								<i class="fa fa-linkedin" aria-hidden="true"></i>
+							</a>
+							<a href="#">
+								<i class="fa fa-youtube" aria-hidden="true"></i>
+							</a>
+                            <FontAwesomeIcon icon={faFacebook} size="4x" className="icon" beat/>
+                <FontAwesomeIcon icon={faTwitter} size="4x" className="icon" beat/>
+                <FontAwesomeIcon icon={faInstagram} size="4x" className="icon" beat/>
+						</li>
+					</div>
+				</div>
+			</div>
+		</div>
+        
+                
+              
                
-            </ul>
-            </div>   
-            
-        </div>
-        <div className="hello"></div>
+               
+
+                
+
+                
+    
+	</footer>
+	<div class="footer-bottom">
+		Copyright © 2022 Group 11, All Rights Reserved
+	</div>
+    	</div>
+	</div>
+
+        
 
         
             
