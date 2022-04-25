3cd03cf3a53501f8de3df6d0bb7ea5de53e846b5
staple2123 <65475509+staple2123@users.noreply.github.com>
2022-04-25 18:15:54 -0500

    track pages no more ghosts
    Reworked how the cards are pulled from DB, now there are no blank spots on the pages

--- client/src/views/pages/nutrition.jsx
+++ client/src/views/pages/nutrition.jsx
@@ -7,6 +7,13 @@ const NutPage = () => {
                    "Each card has a btn that redirects to the itemDetails screen which imports " +
                    "the parameters we use for each unique item.";
     const [items, setItems] = useState([]);
+    const relevant_tracks = [];
+
+    items.forEach(function (item){
+       if (item.track === "nutrition"){
+           relevant_tracks.push(item);
+       }
+    });
 
     useEffect(() => {
         getItems();
@@ -26,11 +33,9 @@ const NutPage = () => {
                 </div>
                 <div className="container">
                     <div className="row">
-                        {items.map((item) => (
+                        {relevant_tracks.map((item) => (
                             <div key={item._id} className="px-5 my-3 col-lg-4 col-md-6 col-sm-12">
-                                { item.track === "nutrition" &&(
-                                    <ItemCard obj={item} />
-                                )}
+                                <ItemCard obj={item} />
                             </div>
                         ))}
                     </div>
