7c5c3af24770ecc474d9ac30104ae603a0686729
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-04-25 15:11:08 -0500

    fixed nutrition ghosts (boo)

--- client/src/views/pages/nutrition.jsx
+++ client/src/views/pages/nutrition.jsx
@@ -1,12 +1,22 @@
 import React, {useEffect, useState} from "react";
 import { getAllItems } from "../../services/itemService"
 import ItemCard from "../../components/ItemCard";
+import {Panorama} from "@material-ui/icons";
+import {PageItem} from "react-bootstrap";
+import {forEach} from "react-bootstrap/ElementChildren";
 
 const NutPage = () => {
     const banner = "This is a test page with item cards that can be reused in other pages. " +
                    "Each card has a btn that redirects to the itemDetails screen which imports " +
                    "the parameters we use for each unique item.";
     const [items, setItems] = useState([]);
+    const relevant_tracks = [];
+
+    items.forEach(function (item){
+       if (item.track === "nutrition"){
+           relevant_tracks.push(item);
+       }
+    });
 
     useEffect(() => {
         getItems();
@@ -26,11 +36,9 @@ const NutPage = () => {
                 </div>
                 <div className="container">
                     <div className="row">
-                        {items.map((item) => (
+                        {relevant_tracks.map((item) => (
                             <div key={item._id} className="px-5 my-3 col-lg-4 col-md-6 col-sm-12">
-                                { item.track === "nutrition" &&(
-                                    <ItemCard obj={item} />
-                                )}
+                                <ItemCard obj={item} />
                             </div>
                         ))}
                     </div>
